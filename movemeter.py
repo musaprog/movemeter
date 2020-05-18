8057536b7e611c690a8113e62f43eeeda9964ca7
Joni <jjtkemppainen1@sheffield.ac.uk>
2020-05-18 11:33:33 +0100

    Add dirty changes for x-ray data analysis

--- movemeter.py
+++ movemeter.py
@@ -89,7 +89,11 @@ class Movemeter:
         if cc_backend == 'OpenCV':
             from .cc_backends.opencv import _find_translation
             self._findTranslation = _find_translation
-        
+       
+        # Movement measure settings
+        self.subtract_previous = False
+        self.tracking_rois = False
+        self.compare_to_first = True
 
     @staticmethod
     def _find_translation(im, im_ref, crop):
@@ -126,6 +130,59 @@ class Movemeter:
         return image.astype(np.float32)
     
 
+    def _measure_movement_optimized_xray_data(self, image_fns, ROIs, max_movement=False):
+        '''
+        Optimized version when there's many rois and subtract previous is True and compare_to_first is False.
+        '''
+        print('Running optimized version for xray data')
+
+        results = []
+
+        # Create a mask image that is subtracted from the images to enhance moving features
+        mask_image = self._imread(image_fns[0]) 
+        for fn in image_fns[1:]:
+            mask_image = np.min([mask_image, self._imread(fn)], axis=0)
+    
+
+        previous_image = self._imread(image_fns[0]) - mask_image
+
+        X = [[0] for roi in ROIs]
+        Y = [[0] for roi in ROIs]
+ 
+        for i, fn in enumerate(image_fns[1:]):
+
+            print('Frame {}/{}'.format(i+1, len(image_fns)))
+           
+            image = self._imread(fn) - mask_image
+            
+            for i_roi, ROI in enumerate(ROIs):
+                print('  _ROI: {}/{}'.format(i+1, len(ROIs)))
+                
+                x, y = self._findTranslation(image, previous_image, ROI,
+                        max_movement=int(max_movement), upscale=self.upscale)
+                    
+                X[i_roi].append(x)
+                Y[i_roi].append(y)
+
+            previous_image = image
+        
+        for x,y in zip(X,Y):
+        
+            x = np.asarray(x)
+            y = np.asarray(y)
+
+            x = x-x[0]
+            y = y-y[0]
+
+            x = np.cumsum(x)
+            y = np.cumsum(y)
+
+            results.append([x.tolist(), y.tolist()])
+    
+        return results
+
+
+
     def _measure_movement(self, image_fns, ROIs, max_movement=False):
         '''
         Generic way to analyse movement using _findTranslation.
@@ -134,28 +191,60 @@ class Movemeter:
         '''
         
         results = []
+     
+        if self.subtract_previous:
+            mask_image = self._imread(image_fns[0])
+            
+            for fn in image_fns[1:]:
+                mask_image = np.min([mask_image, self._imread(fn)], axis=0)
         
         for i, ROI in enumerate(ROIs):
             print('  _measureMovement: {}/{}'.format(i+1, len(ROIs)))
-            previous_image = self._imread(image_fns[0])
+            if self.compare_to_first:
+                previous_image = self._imread(image_fns[0])
 
             X = [0]
             Y = [0]
 
             for i, fn in enumerate(image_fns[1:]):
+                print('ROI IS {}'.format(ROI))
                 print('Frame {}/{}'.format(i+1, len(image_fns)))
-                image = self._imread(fn)
-            
-                x, y = self._findTranslation(image, previous_image, ROI, max_movement=int(max_movement), upscale=self.upscale)
+                
+                #fn = image_fns[3] 
+                #print(fn)
+
+                if self.compare_to_first == False:
+                    print('not comparing to first')
+                    if self.subtract_previous:
+                        previous_image = self._imread(image_fns[i]) -  mask_image
+                    else:
+                        previous_image = self._imread(image_fns[i])
+                
+                if self.subtract_previous:
+                    print('subtracting previous')
+                    image = self._imread(fn) - mask_image
+                else:
+                    image = self._imread(fn)
+                
+                x, y = self._findTranslation(image, previous_image, [int(c) for c in ROI],
+                        max_movement=int(max_movement), upscale=self.upscale)
                     
                 X.append(x)
                 Y.append(y)
+
+                if self.tracking_rois:
+                    print('roi tracking')
+                    #ROI = [ROI[0]+x, ROI[1]+y, ROI[2], ROI[3]]
                 
             X = np.asarray(X)
             Y = np.asarray(Y)
 
             X = X-X[0]
             Y = Y-Y[0]
+
+            if not self.compare_to_first:
+                X = np.cumsum(X)
+                Y = np.cumsum(Y)
 
             results.append([X.tolist(), Y.tolist()])
         
@@ -221,8 +310,13 @@ class Movemeter:
             Ordering is quaranteed to be same as when setting data in Movemeter's setData
         '''
         
-        results = self._measure_movement(self.stacks[stack_i], self.ROIs[stack_i], max_movement=max_movement)
+        if self.subtract_previous == True and self.compare_to_first == False:
+            results = self._measure_movement_optimized_xray_data(self.stacks[stack_i], self.ROIs[stack_i], max_movement=max_movement)
+        else:
+            results = self._measure_movement(self.stacks[stack_i], self.ROIs[stack_i], max_movement=max_movement)
         return results 
+
+
 
 
     # ---------------------------------------------------
